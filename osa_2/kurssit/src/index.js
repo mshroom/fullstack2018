b68d232d770f16e1efa2eb41ea8cda0572be4e9a
mshroom <32199029+mshroom@users.noreply.github.com>
2018-12-07 13:49:02 +0200

    ex 2.5

--- osa_2/kurssit/src/index.js
+++ osa_2/kurssit/src/index.js
@@ -1,37 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Osa = (props) => <p>{props.osa.nimi} {props.osa.tehtavia}</p>
-const Otsikko = (props) => <h1>{props.kurssi.nimi}</h1>
-const Sisalto = (props) => {
-  const osat = props.kurssi.osat
-  return(
-    <div>
-      {osat.map(osa=><Osa key={osa.id} osa={osa}/>)}
-    </div>
-  )
-}
-const Yhteensa = (props) => {
-  const osat = props.kurssi.osat
-  let sum = 0
-  osat.forEach((osa) => {
-    sum += osa.tehtavia
-  })
-  
-  return(
-    <p>yhteensä {sum} tehtävää</p>
-  )
-}
-
-const Kurssi = (props) => {
-    return(
-        <div>
-          <Otsikko kurssi={props.kurssi}/>
-          <Sisalto kurssi={props.kurssi} />
-          <Yhteensa kurssi={props.kurssi}  />
-        </div>
-    )
-}
+import Kurssi from './components/Kurssi'
 
 const App = () => {
     const kurssit = [
