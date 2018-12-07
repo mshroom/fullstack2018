bb1966b4ef52d4cd7e399818d47f3b76496f111c
mshroom <32199029+mshroom@users.noreply.github.com>
2018-12-07 14:43:46 +0200

    ex 2.6

---
+++ osa_2/puhelinluettelo/src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const Person = (props) => <p>{props.person.name}</p>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  addPerson = (event) => {
+      event.preventDefault()
+      const personObject = {
+          name: this.state.newName
+      }
+      const persons = this.state.persons.concat(personObject)
+
+      this.setState({
+          persons,
+          newName: ''
+      })
+      console.log('nappia painettu')
+  }
+
+  handleNewName = (event) => {
+      this.setState({ newName: event.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addPerson}>
+          <div>
+            nimi: <input 
+            value={this.state.newName}
+            onChange={this.handleNewName}
+            />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        <div>
+          {this.state.persons.map(person=><Person key={person.name} person={person} />)}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
