638e6be208e79d4683075fa4e7e50816549ad1ae
mshroom <32199029+mshroom@users.noreply.github.com>
2018-12-07 16:17:50 +0200

    ex 2.11

--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 
 const Person = (props) => {
     return (
@@ -47,12 +48,20 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      persons: [
-        { name: 'Arto Hellas', phone: '0123456'}
-      ],
+      persons: [],
       newName: '',
       newPhone: ''
     }
+  }
+
+  componentDidMount() {
+      console.log('did mount')
+      axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+            console.log('promise fulfilled')
+            this.setState({ persons: response.data })
+        })
   }
 
   addPerson = (event) => {
